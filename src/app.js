5bc65decca5cb0c4507367010ccde5a7b41472f4
khushboothakur2305 <khushboothakur2305@gmail.com>
2022-03-24 12:05:02 +0530

    Chnages

--- src/app.js
+++ src/app.js
@@ -6,11 +6,10 @@ const UserRoutes = require("./routes/user.route");
 const BooksAdmin = require(`./Routes/Books.route`);
 require("./db/db");
 const cors = require("cors");
-const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 app.use("/books", BooksAdmin);
 app.use(UserRoutes);
-app.listen(port, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log("application served");
 });
